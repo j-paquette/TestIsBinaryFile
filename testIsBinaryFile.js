3156edee84787cd1b9bcb769190705cca3e12f7c
j-paquette <56076749+j-paquette@users.noreply.github.com>
2022-03-31 08:39:17 -0400

    still wip, fix code to read all files in fixtures folder and check if they are binary

--- testIsBinaryFile.js
+++ testIsBinaryFile.js
@@ -1,13 +1,13 @@
 //original example: https://www.npmjs.com/package/isbinaryfile
 /*jslint browser:true */
-'use strict';
+/*'use strict';
 
 const isBinaryFile = require('isbinaryfile').isBinaryFile;
 const fs = require("fs");
 
-const filename = "fixtures/2021nov10_AzurePipelines.pdf";
-const data = fs.readFileSync(filename);
-const stat = fs.lstatSync(filename);
+const filenames = "fixtures/2021nov10_AzurePipelines.pdf";
+const data = fs.readFileSync(filenames);
+const stat = fs.lstatSync(filenames);
 
 isBinaryFile(data, stat.size).then((result) => {
   if (result) {
@@ -19,6 +19,56 @@ isBinaryFile(data, stat.size).then((result) => {
 });
 
 const isBinaryFileSync = require('isbinaryfile').isBinaryFileSync;
-const bytes = fs.readFileSync(filename);
-const size = fs.lstatSync(filename).size;
-console.log(isBinaryFileSync(bytes, size)); // true or false 
+const bytes = fs.readFileSync(filenames);
+const size = fs.lstatSync(filenames).size;
+console.log(isBinaryFileSync(bytes, size)); // true or false */
+
+/*jslint browser:true */
+'use strict';
+
+const isBinaryFile = require('isbinaryfile').isBinaryFile;
+const fs = require("fs");
+
+//const dir = "./fixtures";
+const filenames = fs.readdirSync(__dirname);
+
+console.log(filenames);
+
+
+
+filenames.forEach(file => {
+  const data = fs.readFileSync(filenames);
+  const stat = fs.lstatSync(file);
+
+  //console.log(data);
+
+  isBinaryFile(data, stat.size).then((result) => {
+    if (result) {
+      console.log("It is binary!")
+    }
+    else {
+      console.log("No it is not.")
+    }
+  });
+
+  const isBinaryFileSync = require('isbinaryfile').isBinaryFileSync;
+  const bytes = fs.readFileSync(data);
+  const size = fs.lstatSync(file).size;
+  console.log(isBinaryFileSync(bytes, size)); // true or false */
+})
+
+
+
+
+
+
+
+
+//this example uses a promise API instead of callback API: https://blog.logrocket.com/file-processing-node-js-comprehensive-guide/
+/* import * as fsPromises from 'fs/promises';
+
+async function usingPromiseAPI(path) {
+    const promise = fsPromises.readFile(path);
+    console.log('do something else');
+    return await promise;
+} */
